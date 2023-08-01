8b7961a019a86adc136575c171e11f3c5b972329
Piotr Sękowski <piotr.sekowski@capgemini.com>
2023-08-01 16:02:53 +0200

    Section 12

---
+++ src/Section11/L14_Enabled.java
@@ -0,0 +1,44 @@
+package Section11;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class L14_Enabled {
+	
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\psekowsk\\Desktop\\JAVA projects\\Resources\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://teststore.automationtesting.co.uk/login?back=my-account");
+		Thread.sleep(2000);
+	}
+	
+	@Test (enabled = false)
+	public void enterEmail() throws InterruptedException {
+		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	public void enterPassword() throws InterruptedException {
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
+		Thread.sleep(3000);
+	}
+	
+	
+	@AfterSuite
+	public void end() {
+		driver.close();
+		driver.quit();
+	}
+}
